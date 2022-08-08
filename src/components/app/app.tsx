bd4bba9e18dd97ee903df5de2cba7afeaa08f902
Alex <alex1031alex@yandex.ru>
2022-08-08 23:07:07 +0300

    Добавил разметку формы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { FeedbackForm } from "../feedback-form/feedback-form";
 export const App = () => {
   return (
-    <FeedbackForm />
+    <div className="app">
+      <div className="container">
+        <h1 className="app__label">Форма обратной связи</h1>
+        <FeedbackForm />
+      </div>
+    </div>
   );
 };
--- src/components/feedback-form/feedback-form.tsx
+++ src/components/feedback-form/feedback-form.tsx
@@ -3,6 +3,28 @@ import "./feedback-form.scss";
 
 export const FeedbackForm = () => {
   return (
-    <form></form>
+    <form className="feedback-form">
+      <div className="feedback-form__group">
+        <label className="feedback-form__label" htmlFor="feedback-form-name">Имя Фамилия</label>
+        <input className="feedback-form__field" type="text" name="name" id="feedback-form-name" />
+      </div>
+      <div className="feedback-form__group">
+        <label className="feedback-form__label" htmlFor="feedback-form-email">E-mail</label>
+        <input className="feedback-form__field" type="email" name="email" id="feedback-form-email"  />
+      </div>
+      <div className="feedback-form__group">
+        <label className="feedback-form__label" htmlFor="feedback-form-tel">Номер телефона</label>
+        <input className="feedback-form__field" type="tel" name="tel" id="feedback-form-tel" />
+      </div>
+      <div className="feedback-form__group">
+        <label className="feedback-form__label" htmlFor="feedback-form-birth">Дата рождения</label>
+        <input className="feedback-form__field" type="date" name="birth" id="feedback-form-birth" />
+      </div>
+      <div className="feedback-form__group">
+        <label className="feedback-form__label" htmlFor="feedback-form-text">Сообщение</label>
+        <textarea className="feedback-form__field" name="text" id="feedback-form-text" />
+      </div>
+      <button className="feedback-form__btn" type="submit">Отправить</button>
+    </form>
   );
 };
