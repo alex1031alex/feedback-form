ea8b1d0280fe6f9327c9c6cef73b141346a07e58
Alex <alex1031alex@yandex.ru>
2022-08-08 21:46:35 +0300

    Создал исходные папки и файлы проекта

---
+++ src/components/app/app.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { FeedbackForm } from "../feedback-form/feedback-form";
+export const App = () => {
+  return (
+    <FeedbackForm />
+  );
+};
---
+++ src/components/feedback-form/feedback-form.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./feedback-form.scss";
+
+export const FeedbackForm = () => {
+  return (
+    <form></form>
+  );
+};
